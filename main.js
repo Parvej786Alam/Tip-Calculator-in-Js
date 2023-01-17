d6655284bbdbec783ef6e8bf6eee5a8bac58959b
parvej <parvej.alam624@gmail.com>
2023-01-18 01:23:54 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const tip = document.getElementById("range");
+const tit = document.querySelector(".ran");
+
+const tipamt = document.querySelector(".tipamt");
+const amt = document.querySelector(".bill");
+
+
+tip.addEventListener("input", function(e) {
+    let inp=document.getElementById("num").value;
+    let tipam=e.target.value;
+
+    if (inp===""){
+        alert("Amount Required");
+    }else{
+        tit.textContent=`${tipam}%`;
+        tipamt.textContent=`${tipam}%`;
+        amt.textContent=tipCale(parseFloat(inp), parseFloat(tipam));
+    }
+})
+
+function tipCale(amt,tip){
+    let tipc=(amt * tip / 100);
+    return tipc+amt;
+}
